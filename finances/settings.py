f4aec9606e06c97bb5a77bf91ce031c1667152f5
ra2dd <radoslawpryw@gmail.com>
2023-07-11 17:31:51 +0200

    create urls nad make migrations

--- finances/settings.py
+++ finances/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'portfolio.apps.PortfolioConfig'
+    'portfolio.apps.PortfolioConfig',
 ]
 
 MIDDLEWARE = [
