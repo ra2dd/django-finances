6de1fd8f9b4af55e5d268e9b6931735663548159
ra2dd <radoslawpryw@gmail.com>
2023-07-12 12:40:47 +0200

    create Models for portfolio and cash

--- portfolio/models.py
+++ portfolio/models.py
@@ -1,3 +1,39 @@
 from django.db import models
+from django.urls import reverse
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Portfolio(models.Model):
+    owner = models.OneToOneField(
+        User,
+        on_delete=models.CASCADE,
+        primary_key=True
+    )
+
+
+class Cash(models.Model):
+    portfolio = models.ForeignKey(
+        Portfolio, 
+        on_delete=models.SET_NULL,
+    )
+    
+    CURRENCY_TYPE =(
+        ('eur', 'Euro'),
+        ('usd', 'Dollar'),
+    )
+
+    currency = models.CharField(
+        max_length=3,
+        choices=CURRENCY_TYPE,
+    )
+
+
+class CashHistory(models.Model):
+    amount = models.DecimalField(max_digits=19, decimal_places=10)
+
+    date = models.DateField()
+
+    cash = models.ForeignKey(
+        Cash,
+        on_delete=models.SET_NULL,
+    )
+
