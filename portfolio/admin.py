08682ceae7779190be95f31b7c331a8159c415a3
ra2dd <radoslawpryw@gmail.com>
2023-07-12 18:05:12 +0200

    add TabularInlines in admin.py

--- portfolio/admin.py
+++ portfolio/admin.py
@@ -2,22 +2,40 @@ from django.contrib import admin
 from .models import Portfolio, Cash, CashHistory, Stock, StockHistory, Crypto, CryptoHistory
 # Register your models here.
 
-admin.site.register(Portfolio)
-# admin.site.register(Cash)
-# admin.site.register(CashHistory)
 admin.site.register(Stock)
 admin.site.register(StockHistory)
 admin.site.register(Crypto)
 admin.site.register(CryptoHistory)
 
+# Defining inlines
+class CashInline(admin.TabularInline):
+    model = Cash
+    extra = 1
+
+class StockInline(admin.TabularInline):
+    model = Stock
+    extra = 1
+
+class CryptoInline(admin.TabularInline):
+    model = Crypto
+    extra = 1
+
+class CashHistoryInline(admin.TabularInline):
+    model = CashHistory
+    
+# Admin models
+@admin.register(Portfolio)
+class PortfolioAdmin(admin.ModelAdmin):
+    inlines = [CashInline, StockInline, CryptoInline]
+
+@admin.register(Cash)
 class CashAdmin(admin.ModelAdmin):
     list_display = ('portfolio', 'currency')
+    inlines = [CashHistoryInline]
 
-admin.site.register(Cash, CashAdmin)
-
+@admin.register(CashHistory)
 class CashHistoryAdmin(admin.ModelAdmin):
     list_display = ('cash', 'amount', 'date')
-
-admin.site.register(CashHistory, CashHistoryAdmin)
+    list_filter = ('cash',)
 
 
