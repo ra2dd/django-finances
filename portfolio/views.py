2bb0d361ce088ff24fe49fce6f1c6506c9a4c83a
ra2dd <radoslawpryw@gmail.com>
2023-07-12 20:09:35 +0200

    add index view, url, template, add base template

--- portfolio/views.py
+++ portfolio/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from .models import Portfolio, Cash, CashHistory, Stock, StockHistory, Crypto, CryptoHistory
 
-# Create your views here.
+def index(request):
+    """View function for the home page of the site."""
+
+    portfolio_object = Portfolio.objects.all()[0]
+
+    context = {
+        'portfolio_object': portfolio_object,
+    }
+
+    return render(request, 'index.html', context=context)
+
