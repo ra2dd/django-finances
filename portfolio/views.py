92405765c7640e2a6fe3ae29965ed449f726f363
ra2dd <radoslawpryw@gmail.com>
2023-07-13 12:06:36 +0200

    add dashboard template, url and TemplateView

--- portfolio/views.py
+++ portfolio/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from .models import Portfolio, Cash, CashHistory, Stock, StockHistory, Crypto, CryptoHistory
+from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
+from django.views import generic
 
 def index(request):
     """View function for the home page of the site."""
@@ -11,4 +13,24 @@ def index(request):
     }
 
     return render(request, 'index.html', context=context)
+
+
+class DashboardView(generic.TemplateView, LoginRequiredMixin):
+    """View function for the user dashboard"""
+
+    template_name = 'dashboard.html'
+
+    def get_context_data(self):
+
+        user_portfolio = Portfolio.objects.filter(owner=self.request.user)
+        
+        context = {
+            'user_portfolio': user_portfolio,
+        }
+        return context
+
+
+
+
+
 
