0b3265bcffc899812bce42d3dabf545f339490c8
ra2dd <radoslawpryw@gmail.com>
2023-08-09 18:47:22 +0200

    working importing price history data functionality

--- portfolio/tasks/server_tasks.py
+++ portfolio/tasks/server_tasks.py
@@ -1,20 +1,37 @@
-from binance.spot import Spot
 import datetime, json, string
-import os
+import requests, random
 
-from .models import Asset, AssetPriceHistory
+# from portfolio.tasks import server_tasks
+# server_tasks.import_crypto_price_history('xlm')
+headers = {'accept': 'application/json'}
+
+from ..models import Asset, AssetPriceHistory
 
 class PriceHistory():
     def __init__(self, ticker):
         self.ticker = ticker
         self.price = None
 
+
+def datetime_to_timestamp(date):
+    return round(datetime.datetime.timestamp(date))
+
+def timestamp_to_datetime(timestamp):
+
+    timestamp_length = len(str(timestamp))
+
+    if timestamp_length == 13:
+        return datetime.datetime.fromtimestamp(timestamp/1000)
+    elif timestamp_length == 10:
+        return datetime.datetime.fromtimestamp(timestamp)
+    else:
+        raise Exception(f'Timestamp length is out of range - {timestamp_length}')
+
+def contruct_crypto_price_history_url(name, startDate, endDate):
+    return f'https://api.coingecko.com/api/v3/coins/{name.lower()}/market_chart/range?vs_currency=usd&from={startDate}&to={endDate}&precision=6'
+
+
 def import_crypto_price_history(ticker):
-
-    client = Spot(base_url='https://testnet.binance.vision', api_key=os.environ.get('BINANCEAPI_API_KEY', ''), api_secret=os.environ.get('BINANCEAPI_SECRET_KEY', '')) 
-
-    if not client:
-        raise Exception('Error getting crypto price history data, no client.')
     
     asset = Asset.objects.filter(ticker=ticker.upper())
                                  
@@ -26,9 +43,42 @@ def import_crypto_price_history(ticker):
     
     elif len(asset) == 1:
 
-        asset_price_history = AssetPriceHistory.objects.all()
-        print(asset_price_history)
+        if len(asset[0].assetpricehistory_set.all()) > 0:
+            asset_price_history = asset[0].assetpricehistory_set.all()
+
+            '''
+            first_exisitng_record = asset_price_history[0]
+            latest_exisitng_record = asset_price_history.latest()
+            print(first_exisitng_record)
+            print(latest_exisitng_record)
+            '''
+
+            for price_history in asset_price_history:
+                price_history.delete()
+
         #Check latest price history for an asset
+        startDate = datetime.datetime(2023, 5, 1)
+        startDateTimestamp = datetime_to_timestamp(startDate) 
+
+        # endDate = startDate + datetime.timedelta(weeks=1)
+        endDate = datetime.datetime.now()
+        endDateTimestamp = datetime_to_timestamp(endDate) 
+
+        test = 1
+        if test == 1:
+            
+            response = requests.get(contruct_crypto_price_history_url(asset[0].name.lower(), startDateTimestamp, endDateTimestamp), headers=headers)
+            json_response = json.loads(response.content)
+
+            for priceHistory in json_response["prices"]:
+
+                print(f'[{timestamp_to_datetime(priceHistory[0])}, {priceHistory[1]}],\n ')
+
+                new_date = timestamp_to_datetime(priceHistory[0])
+                record = AssetPriceHistory(asset=asset[0], date=new_date, price=priceHistory[1])   
+                record.save()  
+
+
     
 
     
