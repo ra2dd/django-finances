e83e1bd875c198f6c518e40501b1d418471fc6e6
ra2dd <radoslawpryw@gmail.com>
2023-08-09 19:44:59 +0200

    polish price history fetching funct., add comments

--- portfolio/tasks/server_tasks.py
+++ portfolio/tasks/server_tasks.py
@@ -7,12 +7,6 @@ headers = {'accept': 'application/json'}
 
 from ..models import Asset, AssetPriceHistory
 
-class PriceHistory():
-    def __init__(self, ticker):
-        self.ticker = ticker
-        self.price = None
-
-
 def datetime_to_timestamp(date):
     return round(datetime.datetime.timestamp(date))
 
@@ -27,52 +21,46 @@ def timestamp_to_datetime(timestamp):
     else:
         raise Exception(f'Timestamp length is out of range - {timestamp_length}')
 
-def contruct_crypto_price_history_url(name, startDate, endDate):
-    return f'https://api.coingecko.com/api/v3/coins/{name.lower()}/market_chart/range?vs_currency=usd&from={startDate}&to={endDate}&precision=6'
+def contruct_crypto_price_history_url(name, startDate, endDate, precision):
+    return f'https://api.coingecko.com/api/v3/coins/{name.lower()}/market_chart/range?vs_currency=usd&from={startDate}&to={endDate}&precision={precision}'
 
 
-def import_crypto_price_history(ticker):
+def import_crypto_price_history(*argv):
     
-    asset = Asset.objects.filter(ticker=ticker.upper())
-                                 
-    if len(asset) > 1:
-        raise Exception("Cannot fetch data. There is more than one asset with given ticker.")
-    
-    elif len(asset) == 0:
-        raise Exception("Cannot fetch data. There is no asset with given ticker.")
-    
-    elif len(asset) == 1:
-
-        if len(asset[0].assetpricehistory_set.all()) > 0:
-            asset_price_history = asset[0].assetpricehistory_set.all()
-
-            '''
-            first_exisitng_record = asset_price_history[0]
-            latest_exisitng_record = asset_price_history.latest()
-            print(first_exisitng_record)
-            print(latest_exisitng_record)
-            '''
-
-            for price_history in asset_price_history:
-                price_history.delete()
-
-        #Check latest price history for an asset
-        startDate = datetime.datetime(2023, 5, 1)
-        startDateTimestamp = datetime_to_timestamp(startDate) 
-
-        # endDate = startDate + datetime.timedelta(weeks=1)
-        endDate = datetime.datetime.now()
-        endDateTimestamp = datetime_to_timestamp(endDate) 
-
-        test = 1
-        if test == 1:
+    for ticker in argv:
+        asset = Asset.objects.filter(ticker=ticker.upper())
+                                    
+        if len(asset) > 1:
+            raise Exception("Cannot fetch data. There is more than one asset with given ticker.")
+        
+        elif len(asset) == 0:
+            raise Exception("Cannot fetch data. There is no asset with given ticker.")
+        
+        elif len(asset) == 1:
             
-            response = requests.get(contruct_crypto_price_history_url(asset[0].name.lower(), startDateTimestamp, endDateTimestamp), headers=headers)
+            # Check if there are any price history for given assset
+            if len(asset[0].assetpricehistory_set.all()) > 0:
+                asset_price_history = asset[0].assetpricehistory_set.all()
+
+                # Delete all asset price history
+                for price_history in asset_price_history:
+                    price_history.delete()
+
+            # Define the start date and end date of price history fetching
+            startDate = datetime.datetime(2018, 1, 1)
+            startDateTimestamp = datetime_to_timestamp(startDate) 
+
+            endDate = datetime.datetime.now()
+            endDateTimestamp = datetime_to_timestamp(endDate) 
+
+            # Fetch price data from constructed url   
+            response = requests.get(contruct_crypto_price_history_url(asset[0].name.lower(), startDateTimestamp, endDateTimestamp, 8), headers=headers)
             json_response = json.loads(response.content)
 
+            # Create database records from fetched data
             for priceHistory in json_response["prices"]:
 
-                print(f'[{timestamp_to_datetime(priceHistory[0])}, {priceHistory[1]}],\n ')
+                # print(f'[{timestamp_to_datetime(priceHistory[0])}, {priceHistory[1]}],\n ')
 
                 new_date = timestamp_to_datetime(priceHistory[0])
                 record = AssetPriceHistory(asset=asset[0], date=new_date, price=priceHistory[1])   
