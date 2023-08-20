34a407f8ac7508cb75429d6d72a29f29297c7d61
ra2dd <radoslawpryw@gmail.com>
2023-08-20 16:02:22 +0200

    add authentication urls, login template

--- finances/settings.py
+++ finances/settings.py
@@ -1,3 +1,5 @@
+import os
+
 """
 Django settings for finances project.
 
@@ -57,7 +59,7 @@ ROOT_URLCONF = 'finances.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -124,3 +126,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Redirect to home URL after login
+LOGIN_REDIRECT_URL = '/' 
--- finances/urls.py
+++ finances/urls.py
@@ -24,4 +24,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('portfolio/', include('portfolio.urls')),
     path('', RedirectView.as_view(url='portfolio/', permanent=True)),
+    path('accounts/', include('django.contrib.auth.urls')),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
