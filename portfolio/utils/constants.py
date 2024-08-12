fdeba3e6acf574c6348498ad46123f79d761e0df
ra2dd <radoslawpryw@gmail.com>
2024-08-12 18:12:44 +0200

    change database to postgres, update psycopg version,  take START_DATE from env

--- portfolio/utils/constants.py
+++ portfolio/utils/constants.py
@@ -1,5 +1,10 @@
 import datetime
+from decouple import config
 
 # Global constant defining a start date of AssetPriceHistory importing
 # and latest date a user can create AssetBalanceHistory record
-START_DATE = datetime.date(2023, 8, 28)
+START_DATE = datetime.date(
+    int(config("START_DATE_YEAR")),
+    int(config("START_DATE_MONTH")),
+    int(config("START_DATE_DAY"))
+)
