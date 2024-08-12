a5df5e075a2b87f8346a63acf64c5ac82e34b874
ra2dd <radoslawpryw@gmail.com>
2024-08-12 18:12:44 +0200

    change database to postgres, update psycopg version,  take START_DATE from env

--- finances/settings.py
+++ finances/settings.py
@@ -80,8 +80,12 @@ WSGI_APPLICATION = 'finances.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': config("POSTGRES_DB_NAME"),
+        'USER': config("POSTGRES_USER"),
+        'PASSWORD': config("POSTGRES_PASSWORD"),
+        'HOST': 'db',
+        'PORT': '5432'
     }
 }
 
